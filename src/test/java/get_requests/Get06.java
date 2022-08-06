c50b25425081b35eb2211c5264b765fdcb2c5b1f
yasinaniltechpro <105437708+yasinaniltechpro@users.noreply.github.com>
2022-08-06 23:09:46 +0300

    get09

--- src/test/java/get_requests/Get06.java
+++ src/test/java/get_requests/Get06.java
@@ -2,17 +2,21 @@ package get_requests;
 
 import base_urls.HerOkuAppBaseUrl;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
+import org.testng.asserts.SoftAssert;
+
 
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 public class Get06 extends HerOkuAppBaseUrl {
 
      /*
         Given
-            https://restful-booker.herokuapp.com/booking/555
+            https://restful-booker.herokuapp.com/booking/22
         When
             User send a GET request to the URL
         Then
@@ -36,7 +40,7 @@ public class Get06 extends HerOkuAppBaseUrl {
     @Test
     public void get01(){
         //1. Step: Set the Url
-        spec.pathParams("first","booking","second", 555);
+        spec.pathParams("first","booking","second", 22);
         //2. Step: Set the exppected data
 
         //3. Step: Send the request and get the response
@@ -59,17 +63,33 @@ public class Get06 extends HerOkuAppBaseUrl {
                         "bookingdates.checkout",equalTo("2014-10-23"),
                         "additionalneeds",equalTo("Breakfast"));
 
-        //2. Way:(Saturday)
+        //2. Way: We will use JsonPath Class
+        JsonPath json = response.jsonPath();
 
+        assertEquals("Sally", json.getString("firstname"));
+        assertEquals("Brown", json.getString("lastname"));
+        assertEquals(111, json.getInt("totalprice"));
+        assertEquals(true, json.getBoolean("depositpaid"));
+        assertEquals("2013-02-23", json.getString("bookingdates.checkin"));
+        assertEquals("2014-10-23", json.getString("bookingdates.checkout"));
+        assertEquals("Breakfast", json.getString("additionalneeds"));
 
+        //3. Way: Use soft assertion
+        // To use Soft Assertion follow 3 steps
+        // I. Create SoftAssert Object
+        SoftAssert softAssert = new SoftAssert();
 
+        // II. Do Assertion
+        softAssert.assertEquals(json.getString("firstname"),"Sally","Firstname did not match");
+        softAssert.assertEquals(json.getString("lastname"),"Brown","Lastname did not match");
+        softAssert.assertEquals(json.getInt("totalprice"),111,"Total price did not match");
+        softAssert.assertEquals(json.getBoolean("depositpaid"),true,"Deposit paid did not match");
+        softAssert.assertEquals(json.getString("bookingdates.checkin"),"2013-02-23","Checkin did not match");
+        softAssert.assertEquals(json.getString("bookingdates.checkout"),"2014-10-23","Checkout did not match");
+        softAssert.assertEquals(json.getString("additionalneeds"),"Breakfast","Checkout did not match");
 
-
-
-
-
+        // III. Use assertAll() Method. If you do not use  assertAll() method, you will get pass message every time even if they not pass.
+        softAssert.assertAll();
 
     }
-
-
 }
---
+++ src/test/java/get_requests/Get07.java
@@ -0,0 +1,73 @@
+package get_requests;
+
+import base_urls.JsonPlaceHolderBaseUrls;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Get07 extends JsonPlaceHolderBaseUrls {
+
+    /*
+        Given
+	   	    https://jsonplaceholder.typicode.com/todos
+		When
+			 I send GET Request to the URL
+		Then
+			 1)Status code is 200
+			 2)Print all ids greater than 190 on the console
+			   Assert that there are 10 ids greater than 190
+			 3)Print all userIds whose ids are less than 5 on the console
+			   Assert that the number of userIds whose ids are less than 5 is 4
+			 4)Print all titles whose ids are less than 5
+			   Assert that "delectus aut autem" is one of the titles whose id is less than 5
+     */
+
+    @Test
+    public void get01(){
+        //1. Step: Set the Url
+        spec.pathParam("first", "todos");
+
+        //2. Step: Set the expected data
+
+        //3. Step: Send the Request and get the Response
+        Response response = given().spec(spec).when().get("/{first}");
+        response.prettyPrint();
+
+        //4. Step: Do Assertion
+//        1)Status code is 200
+        response.then().assertThat().statusCode(200);
+        //assertEquals(200,response.getStatusCode());
+
+//        2)Print all ids greater than 190 on the console
+
+        JsonPath json = response.jsonPath();
+        List<Integer> ids = json.getList("findAll{it.id>190}.id"); //Groovy Language=Java Based Programming Language
+        System.out.println("ids greater than 190: "+ids);
+
+//        Assert that there are 10 ids greater than 190
+        assertEquals("Number of ids did not match",10, ids.size());
+
+//        3)Print all userIds whose ids are less than 5 on the console
+        List<Integer> userIds =  json.getList("findAll{it.id<5}.userId");
+        System.out.println("userIds whose ids are less than 5: "+userIds);
+//        Assert that the number of userIds whose ids are less than 5 is 4
+        assertEquals("Number of userIds whose ids are less than 5 is not 4",4, userIds.size());
+
+//        4)Print all titles whose ids are less than 5
+        List<String> titles =  json.getList("findAll{it.id<5}.title");
+        System.out.println("titles whose ids are less than 5: "+titles);
+
+//        Assert that "delectus aut autem" is one of the titles whose id is less than 5
+        //1. Way:
+        assertTrue("Expected title is not among them", titles.contains("delectus aut autem"));
+        //2. Way:
+        assertTrue("Expected title is not among them", titles.stream().anyMatch(t->t.equals("delectus aut autem")));
+
+    }
+}
