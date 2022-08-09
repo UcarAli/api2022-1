462bd89e06486eaa587bc5a7027bb857626c2c35
yasinaniltechpro <105437708+yasinaniltechpro@users.noreply.github.com>
2022-08-09 04:49:44 +0300

    day06

---
+++ src/test/java/patch_request/Patch01.java
@@ -0,0 +1,68 @@
+package patch_request;
+
+import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import test_data.JsonPlaceHolderTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+
+public class Patch01 extends JsonPlaceHolderBaseUrl {
+       /*
+        Given
+	        1) https://jsonplaceholder.typicode.com/todos/198
+	        2) {
+                 "title": "Wash the dishes"
+               }
+        When
+	 		I send PATCH Request to the Url
+	    Then
+	   	   Status code is 200
+	   	   And response body is like   {
+									    "userId": 10,
+									    "title": "Wash the dishes",
+									    "completed": true,
+									    "id": 198
+									   }
+     */
+    @Test
+    public void patch01(){
+
+        //1. Step: Set the Url
+        spec.pathParams("first", "todos","second",198);
+
+        //2. Step: Set the expected data
+        JsonPlaceHolderTestData obj = new JsonPlaceHolderTestData();
+        Map<String,Object> expectedData = obj.expectedDataWithMissingKeys(null,"Wash the dishes",null);
+        System.out.println(expectedData);//{title=Wash the dishes}
+
+        //3. Step: Send the Patch Request and get the Response
+        Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().patch("/{first}/{second}");
+        response.prettyPrint();
+
+        //4. Step: Do Assertion
+        //1. Way:
+        response.then().assertThat().statusCode(200).body("title",equalTo(expectedData.get("title")));
+
+        //2. Way: If someone ask you to assert all the body, you can do following:
+        Map<String,Object> expectedDataMapAllKeys = obj.expectedDataWithAllKey(10,"Wash the dishes",true);
+        Map<String ,Object> actualDataMap = response.as(HashMap.class);
+
+        assertEquals(expectedDataMapAllKeys.get("userId"),actualDataMap.get("userId"));
+        assertEquals(expectedDataMapAllKeys.get("title"),actualDataMap.get("title"));
+        assertEquals(expectedDataMapAllKeys.get("completed"),actualDataMap.get("completed"));
+
+        //Or
+
+        response.then().assertThat().statusCode(200).body("title",equalTo(expectedDataMapAllKeys.get("title")),
+                "userId",equalTo(expectedDataMapAllKeys.get("userId")),
+                "completed",equalTo(expectedDataMapAllKeys.get("completed")));
+
+    }
+}
---
+++ src/test/java/post_request/Post03Pojo.java
@@ -0,0 +1,56 @@
+package post_request;
+
+import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.JsonPlaceHolderPojo;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.assertEquals;
+
+public class Post03Pojo extends JsonPlaceHolderBaseUrl {
+
+    /*
+         Given
+            1) https://jsonplaceholder.typicode.com/todos
+            2)  {
+                "userId": 55,
+                "title": "Tidy your room",
+                "completed": false
+                }
+        When
+            I send POST Request to the Url
+        Then
+            Status code is 201
+        And
+            response body is like {
+                                    "userId": 55,
+                                    "title": "Tidy your room",
+                                    "completed": false,
+                                    "id": 201
+                                    }
+     */
+
+    @Test
+    public void post01(){
+        //1. Step: Set the Url
+        spec.pathParam("first","todos");
+
+        //2. Step: Set the expected data
+        JsonPlaceHolderPojo payload = new JsonPlaceHolderPojo(55,"Tidy your room",false);
+        System.out.println("ExpectedData: "+payload);
+
+        //3. Step: Send Post Request and get the Response
+        Response response = given().spec(spec).contentType(ContentType.JSON).body(payload).when().post("/{first}");
+        response.prettyPrint();
+
+        //4. Step: Do Assertion
+        JsonPlaceHolderPojo actualData = response.as(JsonPlaceHolderPojo.class);
+        System.out.println("actualData: "+actualData);
+        assertEquals(payload.getUserId(),actualData.getUserId());
+        assertEquals(payload.getTitle(),actualData.getTitle());
+        assertEquals(payload.getCompleted(),actualData.getCompleted());
+        assertEquals(201,response.getStatusCode());
+    }
+}
